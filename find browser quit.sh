5a369aa99cacddebafd7d7ec09305eb1a2a95f37
quovadimus <quovadimus@users.noreply.github.com>
2015-06-26 16:59:26 -0400

    mkdir -p /private/tmp/browsers
    moved to just if needed

--- find browser quit.sh
+++ find browser quit.sh
@@ -6,23 +6,27 @@ chrome=$(osascript -e 'tell app "System Events" to count processes whose name is
 firefox=$(osascript -e 'tell app "System Events" to count processes whose name is "Firefox"')
 opera=$(osascript -e 'tell app "System Events" to count processes whose name is "Opera"')
 
-mkdir -p /private/tmp/browsers
+
 
 if [[ "$safari" != 0 ]] ; 
 	then
+ 		mkdir -p /private/tmp/browsers
  		touch /private/tmp/browsers/safari
 fi
 
  if [[ "$chrome" != 0 ]] ; 
 	then
+ 		mkdir -p /private/tmp/browsers
  		touch /private/tmp/browsers/chrome
 fi		 
 if [[ "$firefox" != 0 ]] ; 
 	then
+ 		mkdir -p /private/tmp/browsers
  		touch /private/tmp/browsers/firefox
 fi		 
 if [[ "$opera" != 0 ]] ; 
 	then
+ 		mkdir -p /private/tmp/browsers
  		touch /private/tmp/browsers/opera
 		 
 		fi
